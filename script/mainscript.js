d736e2b7006346810f4b4da11f68ac44f2f645b1
Karlsson <emil.karlsson@capgemini.com>
2018-04-11 14:57:08 +0200

    Added auto closing on hamburger button

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-  $(".button").click(function(e){
+$(".button").click(function(e){
     var button = $(this).val();
     if(button === 0){
-      $('html, body').animate({scrollTop: $("#home").offset().top - 60}, 1000)
+      $('#hamburger-btn').trigger('click');
+      $('html, body').animate({scrollTop: $("#home").offset().top}, 1000)
     }
     else if(button === 1){
-      $('html, body').animate({scrollTop: $("#tjänster").offset().top - 60}, 1000)
+      $('#hamburger-btn').trigger('click');
+      $('html, body').animate({scrollTop: $("#services").offset().top}, 1000)
     }
     else if(button === 2){
-      $('html, body').animate({scrollTop: $("#om-oss").offset().top - 60}, 1000)
+      $('#hamburger-btn').trigger('click');
+      $('html, body').animate({scrollTop: $("#about").offset().top}, 1000)
     }
     else if(button === 3){
-      $("html, body").animate({ scrollTop: $('#kontakt').offset().top - 60}, 1000)
+      $('#hamburger-btn').trigger('click');
+      var StopHeight = ($('#contact').prop("scrollHeight") + $('#footer').prop("scrollHeight")) - $(window).height();
+      if(StopHeight > 0){
+        StopHeight = 0;
+      }
+      $('html, body').animate({scrollTop: $("#contact").offset().top + StopHeight}, 1000)
     }
   });
 });
